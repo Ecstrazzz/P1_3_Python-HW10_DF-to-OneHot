035d9e072fe3fa98f7ae4a68038ed99d233962ae
Ecs <ecstrazzz@ya.ru>
2024-06-28 21:56:49 +0500

    Check code

--- task_44.py
+++ task_44.py
@@ -20,3 +20,12 @@ data.head()
 Формат сдачи: ссылка на свой репозиторий
 """
 
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+print(data)
